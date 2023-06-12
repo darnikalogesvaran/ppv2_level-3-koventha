bf92a1902cfc15d97cb23be30de0fc5c83e0e26d
koventha <136299677+koventha@users.noreply.github.com>
2023-06-12 12:43:23 +0530

    Add files via upload

---
+++ koventhan.py
@@ -0,0 +1,16 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:\Users\USER\AppData\Local\Programs\Python\Python311\koventhan.py =
+hello,world!
+what is your name?koventhan
+hello koventhan
+>>> age=12
+>>> print(age)
+12
+>>> weight=45
+>>> print(weight)
+45
+>>> height=155
+>>> print(height)
+155
